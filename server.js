af84079f7d856dd737f0f079badd840d59e627de
Rizki Mufrizal <6929062+RizkiMufrizal@users.noreply.github.com>
2022-09-06 09:30:20 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const os = require("os");
+const https = require("https");
 const app = require("./proxy-server");
 
 const port = process.env.PORT || 8080;
-app.listen(port, () => {
+https.createServer({
+    key: fs.readFileSync("key.pem"),
+    cert: fs.readFileSync("cert.pem")
+}, app).listen(port, () => {
     console.log(`app listening at http://${os.hostname()}:${port}`);
 });
